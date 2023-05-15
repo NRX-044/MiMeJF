correcting some computation miskates

evaluation.py
@@ -19,7 +19,7 @@ def separation_test(ftdist_df,final_sample_list,label_dict):
         sub_idx = [final_sample_list.index(x) for x in sub_sample]
         sub_ftdist = ftdist_df[sub_idx,:]
         sub_collection.append(sub_ftdist)
-    
+
     diff_mat = np.mean(sub_collection[0],axis=0)-np.mean(sub_collection[1],axis=0)
     # it should be of shape (2,)
 
@@ -52,13 +52,13 @@ def rmse_eval(fm,tensor,mat,mask=None):
     rc_tensor = refold(mode2_tenkai,1,shape)
     rc_matrix = np.matmul(fm.tfm[2],fm.mfm.T)
     if mask:
-        rc_tensor = mask[0]*rc_tensor
-        rc_matrix = mask[1]*rc_matrix
-        est_err += np.linalg.norm(tensor-rc_tensor)/np.sqrt(len(tensor))
-        est_err += np.linalg.norm(mat-rc_matrix)/np.sqrt(len(mat))
+        idx_1 = np.nonzero(mask[0])
+        idx_2 = np.nonzero(mask[1])
+        est_err += np.linalg.norm(tensor[idx_1]-rc_tensor[idx_1])/np.sqrt(tensor[idx_1].size)
+        est_err += np.linalg.norm(mat[idx_2]-rc_matrix[idx_2])/np.sqrt(mat[idx_2].size)
     else:
-        est_err += np.linalg.norm(tensor-rc_tensor)/np.sqrt(len(tensor))
-        est_err += np.linalg.norm(mat-rc_matrix)/np.sqrt(len(mat))
+        est_err += np.linalg.norm(tensor-rc_tensor)/np.sqrt(tensor.size)
+        est_err += np.linalg.norm(mat-rc_matrix)/np.sqrt(mat.size)
 
     return est_err
 
@@ -131,8 +131,14 @@ def random_cv(sim_tensor,sim_matrix,tmask,mmask,lf,approach):
     
     inv_tmask = 1-tmask
     inv_mmask = 1-mmask
+    tidx = np.nonzero(tmask)
+    midx = np.nonzero(mmask)
+    itidx = np.nonzero(inv_tmask)
+    imidx = np.nonzero(inv_mmask)
     train_tensor = tmask * sim_tensor 
     train_matrix = mmask * sim_matrix
+
+
 
     shape = list(sim_tensor.shape)
 
@@ -152,16 +158,18 @@ def random_cv(sim_tensor,sim_matrix,tmask,mmask,lf,approach):
     re_trainmode2_tenkai = np.matmul(decomp_fm.tfm[1],decomp_fm.kr4t_product(1).T)
     re_train_tensor = refold(re_trainmode2_tenkai,1,shape)
     re_train_matrix = np.matmul(decomp_fm.tfm[2],decomp_fm.mfm.T)
-    #re_train_tensor = tmask*re_train_tensor
-    #re_train_matrix = mmask*re_train_matrix
+
     
-    train_rce = np.linalg.norm(tmask*(sim_tensor-re_train_tensor))/np.linalg.norm(tmask*sim_tensor)
-    train_rce += np.linalg.norm(mmask*(sim_matrix-re_train_matrix))/np.linalg.norm(mmask*sim_matrix)
-    print("final rce of train set is: ",train_rce)
+    weight_tensor = sim_tensor.size/(sim_tensor.size+sim_matrix.size)
+    weight_matrix = sim_matrix.size/(sim_tensor.size+sim_matrix.size)
+
+    train_rce = weight_tensor*np.linalg.norm(sim_tensor[tidx]-re_train_tensor[tidx])/np.linalg.norm(sim_tensor[tidx])
+    train_rce += weight_matrix*np.linalg.norm(sim_matrix[midx]-re_train_matrix[midx])/np.linalg.norm(sim_matrix[midx])
+
     
     #compute the RMSE of test set        
-    test_rce = np.linalg.norm(inv_tmask*(sim_tensor-re_train_tensor))/np.linalg.norm(inv_tmask*sim_tensor)
-    test_rce += np.linalg.norm(inv_mmask*(sim_matrix--re_train_matrix))/np.linalg.norm(inv_mmask*sim_matrix)
-    print("final rce of test set is: ",test_rce)
+    test_rce = weight_tensor*np.linalg.norm(sim_tensor[itidx]-re_train_tensor[itidx])/np.linalg.norm(sim_tensor[itidx])
+    test_rce += weight_matrix*np.linalg.norm(sim_matrix[imidx]-re_train_matrix[imidx])/np.linalg.norm(sim_matrix[imidx])
+
         
     return train_rce,test_rce

mmjf.py
@@ -54,11 +54,17 @@ class BasicFac(factor_matrix):
             print("error:",te)
 
         if not self.mask:
-            res = joint_mt(self.tensor,self.matrix,self.lf)
+            res = joint_mt(self.tensor,
+                           self.matrix,
+                           self.lf)
         else:
             tmask = self.mask[0]
             mmask = self.mask[1]
-            res = joint_mt(self.tensor,self.matrix,self.lf,tmask,mmask)
+            res = joint_mt(self.tensor,
+                           self.matrix,
+                           self.lf,
+                           tmask,
+                           mmask)
         self.tfm = res.tfm
         self.mfm = res.mfm
 
@@ -145,4 +151,34 @@ class StructRevealFac(factor_matrix):
         except TypeError as te:
             print("error:",te)
         
+        if not self.mask:
+            res = all_in_one_mt(self.tensor,
+                                self.matrix,
+                                self.lf,
+                                self.cparam)
+        else:
+            tmask = self.mask[0]
+            mmask = self.mask[1]
+            res =  all_in_one_mt(self.tensor,
+                                self.matrix,
+                                self.lf,
+                                self.cparam,
+                                tmask,
+                                mmask)
+        tshape = list(self.tensor.shape)
+        mshape = list(self.matrix.shape)
+        self.tfm = [np.zeros((tshape[x],self.lf),dtype=float) for x in range(len(tshape))]
+        cnt_idx = 0
+        for d in range(len(tshape)):
+            if d < 1:
+                self.tfm[d] = res.x[0:(tshape[d]*self.lf)].reshape(tshape[d],self.lf)
+            else:
+                cnt_idx+=tshape[d-1]*self.lf
+                self.tfm[d] = res.x[cnt_idx:(cnt_idx+tshape[d]*self.lf)].reshape(tshape[d],self.lf)
+        cnt_idx+=tshape[-1]*self.lf
+        self.mfm = res.x[cnt_idx:(cnt_idx+mshape[-1]*self.lf)].reshape(mshape[-1],self.lf)
+        cnt_idx+=mshape[-1]*self.lf
+        self.weight.append(res.x[cnt_idx:cnt_idx+self.lf].reshape(1,self.lf).squeeze())
+        cnt_idx+=self.lf
+        self.weight.append(res.x[cnt_idx:cnt_idx+self.lf].reshape(1,self.lf).squeeze())
         

optimization.py
@@ -31,7 +31,6 @@ def als_optimize(fm,
             V = V + np.matmul(fm.mfm.T,fm.mfm)
         pinverseV = np.matmul(np.linalg.pinv(np.matmul(V.T,V)),V.T)
         F = np.matmul(np.matmul(comp_unfold,kr_product.T),pinverseV)
-        #F = np.matmul(M1,pinverseV)
         return F
     else:
         M = np.matmul(mb_df.T,np.linalg.pinv(fm.tfm[2]).T)
@@ -51,7 +50,7 @@ def funcgrad_gen(mm_tensor,
 
     
     def obj_f(x,*args):
-        mm_tensor,mb_mat,alpha,beta_ten,beta_mat,eps,lf = args
+        mm_tensor,mb_mat,alpha,beta_ten,beta_mat,eps,mask,lf = args
         shape = list(mm_tensor.shape)
         mshape = list(mb_mat.shape)
         
@@ -71,12 +70,16 @@ def funcgrad_gen(mm_tensor,
         cnt_idx+=lf
         mweight = x[cnt_idx:cnt_idx+lf].reshape(1,lf)        
         
-        '''reconstruct tensor by factor matrices and weights of components'''
+        #reconstruct tensor by factor matrices and weights of components
         mode2_tenkai = np.matmul(tfm[1]*tweight,raw_krp(tfm[2],tfm[0]).T)
         rc_tensor =refold(mode2_tenkai,1,shape)
-        '''reconstruct matrix by factor matrices and weights of components'''
+        #reconstruct matrix by factor matrices and weights of components
         rc_matrix = np.matmul(tfm[2]*mweight,mfm.T)
-        ''' add 0.5 multiplication for derivative computation efficiency'''
+        if mask:
+            rc_tensor = mask[0]*rc_tensor
+            rc_matrix = mask[1]*rc_matrix
+            
+        #add 0.5 multiplication for derivative computation efficiency
         obj_func = 0.5*np.square(np.linalg.norm(mm_tensor))
         -np.sum(np.multiply(mm_tensor,rc_tensor)) 
         + 0.5*np.square(np.linalg.norm(rc_tensor))
@@ -98,7 +101,7 @@ def funcgrad_gen(mm_tensor,
     #each element except last two in grad is a numpy array of shape(feature_length,r),the last two elements are  arrays of shape(,r)
        
     def total_grad(x,*args):
-        mm_tensor,mb_mat,alpha,beta_ten,beta_mat,eps,lf = args
+        mm_tensor,mb_mat,alpha,beta_ten,beta_mat,eps,mask,lf = args
         shape = list(mm_tensor.shape)
         mshape = list(mb_mat.shape)
         
@@ -179,7 +182,8 @@ def funcgrad_gen(mm_tensor,
 #prototype function for coupling microbes-predicted functional pathways 3-order tensor and metabolites matrix
 #return normalized unit length factor matrices of each dimensions and corresponding weight.
 def joint_mt(mm_tensor,
-             mb_mat,lf,
+             mb_mat,
+             lf,
              tmask=np.array([]),
              mmask=np.array([])):
     #first initialize factorized rank-1 tensors and matrices using SVD
@@ -256,14 +260,22 @@ def joint_mt(mm_tensor,
 ## BMC Bioinformatics 15, 239 (2014). https://doi.org/10.1186/1471-2105-15-239
 def all_in_one_mt(mm_tensor,
                   mb_mat,
-                  lf):
+                  lf,
+                  cparam,
+                  tmask=np.array([]),
+                  mmask=np.array([])):
     #get the initial values first
     try: 
         if len(mm_tensor.shape) != 3:
             raise ValueError("only accept 3-way tensor as input")
     except ValueError as ve:
         print("error:",ve)
-           
+
+    if tmask.size and mmask.size:
+        mask = [tmask,mmask]
+    else:
+        mask = []
+
     init_fm = factor_matrix()
     init_fm.tfm = []
     #SVD-based factor matrices initialization
@@ -302,18 +314,18 @@ def all_in_one_mt(mm_tensor,
     
     
     # generate objective function and corresponding gradients 
-    func,grad = funcgrad_gen(mm_tensor,mb_mat,init_fm)
+    func,grad = funcgrad_gen(mm_tensor,mb_mat,init_fm,mask)
     print("obj function and gradient generation completed\n")
     
     
     
     
     # the constant argument in model
-    ALPHA = 1
-    BETA_TEN = 1e-3
-    BETA_MAT = 1e-3
-    EPS = 1e-8
-    ARGS = (mm_tensor,mb_mat,ALPHA,BETA_TEN,BETA_MAT,EPS,lf)
+    ALPHA = cparam['alpha']
+    BETA_TEN = cparam['beta_ten']
+    BETA_MAT = cparam['beta_mat']
+    EPS = cparam['eps']
+    ARGS = (mm_tensor,mb_mat,ALPHA,BETA_TEN,BETA_MAT,EPS,mask,lf)
 
     OPTS = {'maxiter' : None,
             'disp' : True,